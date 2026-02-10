f96e8d71efbf5a65bbf1d5f64f5c9d3ce88fd83e
LNU <78723629+ian20040409@users.noreply.github.com>
2026-02-11 03:05:13 +0800

    Delete build directory

--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/network_info_plus.build/Objects-normal/arm64/network_info_plus_vers.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/linenyou/Github/LocalNetSpeed-flutter/build/macos/Build/Intermediates.noindex/Pods.build/Debug/network_info_plus.build/module.modulemap \
-  /Users/linenyou/Github/LocalNetSpeed-flutter/build/macos/Build/Intermediates.noindex/Pods.build/Debug/network_info_plus.build/DerivedSources/network_info_plus_vers.c
